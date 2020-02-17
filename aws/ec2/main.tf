64a11add0b15d458e32e5ea10acc5d53efc3b105
esilveira86 <ewerton.silveira@gmail.com>
2020-02-17 06:33:36 -0300

    Ajusteno security group

--- aws/ec2/main.tf
+++ aws/ec2/main.tf
@@ -13,6 +13,12 @@ resource "aws_security_group" "web_server" {
         protocol    = "tcp" 
         cidr_blocks = ["0.0.0.0/0"]    
     }
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
     tags = {
         Name = "web-server"
     }
@@ -23,7 +29,7 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.amazon.id
   instance_type = "t2.micro"
   key_name = var.key_name
-  vpc_security_group_ids = ["${aws_security_group.web_server.id}"]
+  security_groups = ["${aws_security_group.web_server.name}"]
   user_data = <<EOT
         #!/bin/bash
         yum update -y
--- aws/ec2/outputs.tf
+++ aws/ec2/outputs.tf
@@ -3,6 +3,11 @@ output "public_ip" {
   value       = aws_instance.web.public_ip
 }
 
+output "private_ip" {
+  description = "Lista de endereços IP privados atribuídos a instância."
+  value       = aws_instance.web.public_ip
+}
+
 output "public_dns" {
   description = "Lista de nomes DNS públicos atribuídos à instaância."
   value       = aws_instance.web.public_dns
